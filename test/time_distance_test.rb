45c32964f87654e7545318d8dc9e8542398799e2
Manuel Gomes <manuel@sherlock.local>
2021-07-21 12:35:56 +1000

    Tabula rasa

---
+++ test/time_distance_test.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TimeDistanceTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::TimeDistance::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
